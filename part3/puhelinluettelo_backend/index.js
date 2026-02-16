a9196005cd6034349f707ebd51b3a79dcad2132b
Ahiisivi <arttu.hiisivirta@student.hamk.fi>
2026-02-16 11:32:54 +0200

    part3: 3.9 puhelinluettelo backend step 9

--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -9,6 +10,8 @@ morgan.token('body', (req) => {
 app.use(express.json())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(cors())
 
 let persons = [
     { 
@@ -104,7 +107,7 @@ app.post('/api/persons/', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
