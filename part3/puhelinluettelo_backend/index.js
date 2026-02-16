e0663ef84c0880ca97731eb0ce238d326704851e
Ahiisivi <arttu.hiisivirta@student.hamk.fi>
2026-02-16 10:14:15 +0200

    part3: 3.7 puhelinluettelo backend step 8

--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello from puhelinluettelo backend</h1>')
